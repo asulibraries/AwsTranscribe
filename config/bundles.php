7e88e7e5ed5e4181e9d79c136b52d76f9d824642
Seth Shaw <sshaw28@asu.edu>
2022-10-13 16:03:21 -0700

    *actually* purge monolog

--- config/bundles.php
+++ config/bundles.php
@@ -3,5 +3,4 @@
 return [
     Aws\Symfony\AwsBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
