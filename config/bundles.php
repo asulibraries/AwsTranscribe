a57e42e26c637fd651413628f3e1d8df385c1350
Seth Shaw <sshaw28@asu.edu>
2022-10-13 13:24:50 -0700

    add monolog properly

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Aws\Symfony\AwsBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
